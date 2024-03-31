5dbdacf2f632dfe854096a105d89d1d2fc957850
eldar <eldar@ASER_ASPIRE_7E>
2024-03-31 17:11:10 +0300

    Задача 2 с импульсной партией

--- quasi_stationary_models/quasi_stationary_models.cpp
+++ quasi_stationary_models/quasi_stationary_models.cpp
@@ -31,14 +31,14 @@ TEST(Block_3, Task_QP_1)
 	ring_buffer_t <std::vector<std::vector<double>>> buffer(number_layers_buffer, { initial_density_layer, initial_viscosity_layer });
 	/// @param Число рассчитываемых параметров (2 параметра - сера и вязкость)
 	int num_parameters = 2;
-	/// @param Начальная плотность сырья в трубе, [кг/м3]
+	/// @param Плотность закачиваемой партии, [кг/м3]
 	double density = 800;
-	/// @param Начальная вязкость сырья в трубе, [м2/с]
+	/// @param Вязкость закачиваемой партии, [м2/с]
 	double viscosity = 10e-6;
 	/// @param Вектор параметров нефти входных партий
 	vector <double> input_conditions(num_parameters);
 	input_conditions = { density, viscosity };
-	/// @param Время моделирования
+	/// @param Время моделирования, [c]
 	double T = 200000;
 	/// @param j - счетчик слоев
 	int j = 0;
@@ -64,7 +64,7 @@ TEST(Block_3, Task_QP_1)
 
 
 
-/// Задание 01. Задача QP. Учет различия временных сеток краевых условий и метода характеристик.
+/// Задание 3. Задача QP. Учет различия временных сеток краевых условий и метода характеристик.
 TEST(Block_3, Task_QP_01) {
 	/// Предполагаем, что в начальный момент времени всю трубу заполняют нефть с начальными параметрами initial_density, initial_viscosity
 	/// @param Структура параметров трубы задачи 2
@@ -82,7 +82,7 @@ TEST(Block_3, Task_QP_01) {
 	/// @param Синтетическая матрица параметров нефти входных партий
 	vector <vector <double>> input_conditions(3, vector<double>(synthetic_time_2.density.size()));
 	input_conditions = { synthetic_time_2.density, synthetic_time_2.viscosity, synthetic_time_2.time_input_parties };
-	/// @param Время моделирования
+	/// @param Время моделирования, [c]
 	double T = 4000;
 	/// @param j - счетчик слоев
 	int j = 0;
@@ -125,3 +125,59 @@ TEST(Block_3, Task_QP_01) {
 	} while (sum_dt <= T);
 }
 
+///// Задание 2. Задача QP с «импульсной» партией.Вытеснение начальной партии другой партией при неизменном расходе перекачки, «импульсная» партия полностью не замещает основную.
+TEST(Block_3, Task_QP_2)
+{
+	/// Предполагаем, что в начальный момент времени всю трубу заполняют нефть с начальными параметрами initial_density, initial_viscosity
+	/// @param Структура параметров трубы задачи 2
+	Input_data input_data_task_2;
+	/// @param Начальный слой по плотности (начальное заполнение трубы)
+	std::vector <double> initial_density_layer(input_data_task_2.n, input_data_task_2.initial_density);
+	/// @param Начальный слой по вязкости (начальное заполнение трубы)
+	std::vector <double> initial_viscosity_layer(input_data_task_2.n, input_data_task_2.initial_viscosity);
+	/// @param number_layers_buffer - количество слоев в буфере (для метода характеристик достаточно хранить 2 слоя - предыдущий и текущий слои)
+	int number_layers_buffer = 2;
+	ring_buffer_t <std::vector<std::vector<double>>> buffer(number_layers_buffer, { initial_density_layer, initial_viscosity_layer });
+	/// @param Число рассчитываемых параметров (2 параметра - сера и вязкость)
+	int num_parameters = 2;
+	/// @param Плотность закачиваемой партии, [кг/м3]
+	double density = 990;
+	/// @param Вязкость закачиваемой партии, [м2/с]
+	double viscosity = 19e-6;
+	/// @param Вектор параметров нефти входных партий - импульсная партия 
+	vector <double> input_conditions(num_parameters);
+	input_conditions = { density , viscosity };
+	/// @param Вектор параметров нефти входных партий - начальная (исходная) партия
+	vector <double> original_conditions(num_parameters);
+	original_conditions = { input_data_task_2.initial_density, input_data_task_2.initial_viscosity };
+	/// @param Время моделирования, [c]
+	double T = 200000;
+	/// @param Время закачивания импульсной партии, [c]
+	double Tin = 5 * 3600;
+	/// @param j - счетчик слоев
+	int j = 0;
+	double sum_dt = 0;
+	Synthetic_time_series synthetic_time_2;
+	/// @param Вектор распределения давления по трубе, [Па]
+	std::vector <double> pressure_current(input_data_task_2.n);
+	do {
+		Transport_equation transport_equation_task_1(input_data_task_2, synthetic_time_2, j);
+		if (sum_dt <= Tin) {
+			for (size_t i{ 0 }; i < num_parameters; i++) {
+			transport_equation_task_1.method_characteristic(buffer.current()[i], buffer.previous()[i], input_conditions[i]);
+			};
+		}
+		else {
+			for (size_t i{ 0 }; i < num_parameters; i++) {
+				transport_equation_task_1.method_characteristic(buffer.current()[i], buffer.previous()[i], original_conditions[i]);
+			};
+		}
+		/// Передаю текущий слой
+		Count_pressure_Eyler_Task_1 count_pressure_Eyler_task_1(input_data_task_2, buffer.current()[0], buffer.current()[1], pressure_current);
+		pressure_current = count_pressure_Eyler_task_1.count_pressure_Eyler();
+		transport_equation_task_1.output_data("Output_task_2", buffer, sum_dt, pressure_current);
+		buffer.advance(1);
+		sum_dt += input_data_task_2.get_dt();
+		j++;
+	} while (sum_dt <= T);
+}
